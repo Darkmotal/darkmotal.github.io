80633a502bcd727c7da3c65e087d8e776d821b0d
Darkmotal <darkmotal@gmail.com>
2020-12-22 16:20:04 +0200

    Add files via upload

--- ripoll/assets/app.js
+++ ripoll/assets/app.js
@@ -57,10 +57,48 @@ $(function(){
    
     $("#menu").on("click", function(){
         $("#mobMenu").addClass("open");
-    })
+    });
     
     $("#close").on("click", function(){
         $("#mobMenu").removeClass("open");
-    })
+    });
+    
+    
+    $("#slider").on("scroll", function(){
+        var slideW = $(this).innerWidth(),
+            hScroll = $(this).scrollLeft();
+        
+        if (hScroll >= 2 * slideW) {
+            $("#s3").addClass("active");
+            $("#s2").removeClass("active");
+            $("#s1").removeClass("active");
+        } else if (hScroll >= slideW){
+            $("#s2").addClass("active");
+            $("#s3").removeClass("active");
+            $("#s1").removeClass("active");
+        } else {
+            $("#s1").addClass("active");
+            $("#s2").removeClass("active");
+            $("#s3").removeClass("active");
+        }
+        
+    });
+    
+    $(".selector").on("click", function(){
+        var slideW = $("#slider").innerWidth(),
+            selectorId = $(this).attr('id');
+        
+        switch (selectorId) {
+                case "s1" : $("#slider").scrollLeft(slideW/2);
+                break;
+                case "s2" : $("#slider").scrollLeft(slideW);
+                break;
+                case "s3" : $("#slider").scrollLeft(slideW * 2);
+                break;
+        }
+        
+    });
+    
+    
 });
 
